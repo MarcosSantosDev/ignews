a2095cb4c875f86fe5cd758c3ae52a09382319ab
MarcosSantosDev <marcossantos@ioasys.com.br>
2022-04-13 21:12:22 -0300

    feat: updates
    - rename file to use typescript;
    - create component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" alt="ig.news" width="100%" height="100%"/>
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
