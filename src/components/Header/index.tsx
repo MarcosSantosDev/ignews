3bf261442064cbec3fa1a3732b0ccc7a70d356f2
MarcosSantosDev <marcossantos@ioasys.com.br>
2022-04-13 22:06:00 -0300

    feat: remove Image next tag

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
@@ -6,7 +5,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" alt="ig.news" width="100%" height="100%"/>
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
