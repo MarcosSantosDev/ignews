2ad6ab5f900a7a6937ef7cdcc75cc47fad1e1a3c
MarcosSantosDev <marcossantos@ioasys.com.br>
2022-04-13 22:13:21 -0300

    feat: subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
 
 export default function Home() {
@@ -15,6 +16,8 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
