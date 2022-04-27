31b9aa0d9431a04443b750ae41bfb6ef88bd9b98
MarcosSantosDev <marcossantos@ioasys.com.br>
2022-04-26 22:54:56 -0300

    feat(faunadb): setup
    - setup faunadb;
    - save user in faunadb;

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,14 @@
 
+import faunadb from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { faunaClient } from "../../../services/fauna"
 
 export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID_KEY,
+      clientSecret: process.env.GITHUB_SECRET_KEY,
       authorization: {
         params: {
           scope: 'read:user'
@@ -14,4 +16,38 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await faunaClient.query(
+          faunadb.query.If(
+            faunadb.query.Not(
+              faunadb.query.Exists(
+                faunadb.query.Match(
+                  faunadb.query.Index('user_by_email'),
+                  faunadb.query.Casefold(user.email)
+                )
+              )
+            ),
+            faunadb.query.Create( 
+              faunadb.query.Collection('users'),
+              { data: { email } }
+            ),
+            faunadb.query.Get(
+              faunadb.query.Match(
+                faunadb.query.Index('user_by_email'),
+                faunadb.query.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false;
+      }
+    },
+  }
 })
