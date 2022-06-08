518f4140e44e8bb403871643baf76317ea296141
MarcosSantosDev <marcosadriano034@gmail.com>
2022-06-07 22:55:24 -0300

    feat: create ActiveLink component

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,22 @@
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
+import { cloneElement } from 'react';
+
+type ActiveLinkProps = LinkProps & {
+  children: React.ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink({ children, activeClassName, ...rest }: ActiveLinkProps) {
+  const { asPath } = useRouter();
+  
+  const className = asPath === rest.href ? activeClassName : '';
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className
+      })}
+    </Link>
+  )
+}
